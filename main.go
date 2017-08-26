64a42fb6016e255219435cd9f8e8057b427b6d7c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-08-26 11:01:46 +0900

    run API only once
    "issues" includes "PRs"

--- main.go
+++ main.go
@@ -8,26 +8,17 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
+	const issueTitle = "[Issue]"
+	const prTitle = "[PR]"
 
+	var title string
+
+	ctx := context.Background()
 	client := github.NewClient(nil)
 	repos := []string{"minitest-retry", "travel_base"}
 
 	for _, repo_name := range repos {
 		fmt.Printf("**** %s ****\n", repo_name)
-		prs, _, err := client.PullRequests.List(ctx, "y-yagi", repo_name, nil)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-			continue
-		}
-
-		if len(prs) > 0 {
-			fmt.Printf("PRs:\n")
-			for _, pr := range prs {
-				fmt.Printf("%s: %s\n", pr.GetTitle(), pr.GetHTMLURL())
-			}
-			fmt.Printf("\n")
-		}
 
 		issues, _, err := client.Issues.ListByRepo(ctx, "y-yagi", repo_name, nil)
 		if err != nil {
@@ -36,11 +27,14 @@ func main() {
 		}
 
 		if len(issues) > 0 {
-			fmt.Printf("Issues:\n")
 			for _, issue := range issues {
 				if issue.PullRequestLinks == nil {
-					fmt.Printf("%s: %s\n", issue.GetTitle(), issue.GetHTMLURL())
+					title = issueTitle
+				} else {
+					title = prTitle
 				}
+
+				fmt.Printf("%s %s: %s\n", title, issue.GetTitle(), issue.GetHTMLURL())
 			}
 			fmt.Printf("\n")
 		}
